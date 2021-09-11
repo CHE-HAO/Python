ec64c40d4fa7947d3b4c3a11318f623a377d1713
Justin <justinlin0529@gmail.com>
2021-09-11 08:29:46 +0800

    1. 調整排版

--- SharefieCrawler.py
+++ SharefieCrawler.py
@@ -10,34 +10,24 @@ from bs4 import BeautifulSoup
 start = 5201
 end = 5250
 
-for i in range(start, end):
-
-    url = 'https://www.sharefie.net/post/{0}'.format(i)
-    # print(url)
-
-    resp = requests.get(url)
-    # print(resp)
-
-    if resp.status_code == 200:
-
-        soup = BeautifulSoup(resp.text, 'html.parser')
-        # print(soup)
-
-        not_found = soup.select("div#maincont div.pagenotfound")
-        # print(not_found)
-
-        if len(not_found) == 0:
-
-            try:
-                selector = soup.select("div#maincont div#postcontent img")
-                # print(selector)
-
-                for select in selector:
-
-                    img = select["data-src"]
-                    if img.startswith("http"):
-                        print(img)
-
-            except KeyError:
-                continue
-
+if __name__ == '__main__':
+    for i in range(start, end):
+        url = 'https://www.sharefie.net/post/{0}'.format(i)
+        # print(url)
+        resp = requests.get(url)
+        # print(resp)
+        if resp.status_code == 200:
+            soup = BeautifulSoup(resp.text, 'html.parser')
+            # print(soup)
+            not_found = soup.select("div#maincont div.pagenotfound")
+            # print(not_found)
+            if len(not_found) == 0:
+                try:
+                    selector = soup.select("div#maincont div#postcontent img")
+                    # print(selector)
+                    for select in selector:
+                        img = select["data-src"]
+                        if img.startswith("http"):
+                            print(img)
+                except KeyError:
+                    continue
